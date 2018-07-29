cbc8bbdeea4ce19a913f09a3afd53928036bc533
bodziowagh <k.wolinski@univapay.com>
2018-07-29 23:29:03 +0900

    [start]

---
+++ src/index.js
@@ -0,0 +1,167 @@
+const SCENE = {
+	FRUSTUM: 75,
+	WIDTH: window.innerWidth,
+	HEIGHT: window.innerHeight,
+	NEAR: 0.01,
+	FAR: 100
+};
+
+const FLOOR = {
+	SIZE: 20,       // Remember to make it dividable by 2
+	COLOR: 0x00ff22
+};
+
+const CONFIG = {
+    displayDirectionsIndicator: false,
+    camera: {
+        autoRotate: true
+    }
+};
+
+let renderer, camera, scene, controls;
+let floor;
+
+function init() {
+	function initWebGL() {
+		renderer = new THREE.WebGLRenderer();
+		camera = new THREE.PerspectiveCamera(SCENE.FRUSTUM,
+											 SCENE.WIDTH / SCENE.HEIGHT,
+											 SCENE.NEAR,
+											 SCENE.FAR);
+		scene = new THREE.Scene();
+
+		camera.position.set(15, 8, 15);             // TODO: define those as props
+		camera.lookAt(new THREE.Vector3(0, 0, 0));
+
+        controls = new THREE.OrbitControls(camera);
+        controls.autoRotate = CONFIG.camera.autoRotate;
+        controls.addEventListener("change", render);
+
+		scene.add(camera);
+
+		renderer.setSize(SCENE.WIDTH, SCENE.HEIGHT);
+		renderer.shadowMap.enabled = true;
+		renderer.shadowMap.type = THREE.PCFShadowMap;
+
+		document.body.appendChild(renderer.domElement);
+	}
+
+	function initIndicator() {
+		const geometryY = new THREE.Geometry();
+        const materialY = new THREE.LineBasicMaterial( { color: 0xff0000 } );
+        const verticesY = [
+			new THREE.Vector3(0, 1, 0),
+			new THREE.Vector3(0, 0, 0)
+		];
+        geometryY.vertices = verticesY;
+
+        const geometryX = new THREE.Geometry();
+        const materialX = new THREE.LineBasicMaterial( { color: 0x00ff00 } );
+        const verticesX = [
+            new THREE.Vector3(1, 0, 0),
+            new THREE.Vector3(0, 0, 0)
+        ];
+        geometryX.vertices = verticesX;
+
+        const geometryZ = new THREE.Geometry();
+        const materialZ = new THREE.LineBasicMaterial( { color: 0x0000ff } );
+        const verticesZ = [
+            new THREE.Vector3(0, 0, 1),
+            new THREE.Vector3(0, 0, 0)
+        ];
+        geometryZ.vertices = verticesZ;
+
+        scene.add(new THREE.Line(geometryY, materialY));
+        scene.add(new THREE.Line(geometryX, materialX));
+        scene.add(new THREE.Line(geometryZ, materialZ));
+	}
+
+	function initFloor() {
+		const geometry = new THREE.Geometry();	// TODO change to buffere geometry
+		const material = new THREE.PointsMaterial({
+			size: 2,
+			sizeAttenuation: false,
+			color: FLOOR.COLOR
+		});
+
+        for (let x = -FLOOR.SIZE / 2; x < FLOOR.SIZE / 2; x++) {
+            for (let z = -FLOOR.SIZE / 2; z < FLOOR.SIZE / 2; z++) {
+                geometry.vertices.push(new THREE.Vector3(x, 0, z));
+            }
+        }
+
+        floor = new THREE.Points(geometry, material);
+		scene.add(floor);
+    }
+
+
+	function initLight() {
+		const light = new THREE.PointLight( 0xffffff, 1, 50 );
+
+		light.position.set(0, 0, 0);
+
+		light.castShadow = true;
+		light.shadow.camera.near = 1;
+		light.shadow.camera.far = 25;
+
+		scene.add(light);
+	}
+
+	initWebGL();
+    CONFIG.displayDirectionsIndicator && initIndicator();
+	initLight();
+
+	initFloor();
+}
+
+function render() {
+    renderer.render(scene, camera);
+}
+
+
+function getIncrementValue(currentY, targetY) {
+    const slowDownStart = targetY * 0.9;
+    const speed = targetY / 200;
+
+    if (currentY < slowDownStart) {
+        return speed;
+    }
+
+    if (currentY < targetY) {
+        return speed - ((currentY - slowDownStart) / (targetY - slowDownStart) * speed);
+    }
+
+    return 0;
+}
+
+function animate() {
+    // Animate floor:
+	for (let x = 0; x < FLOOR.SIZE; x++) {
+	    for (let z = 0; z < FLOOR.SIZE; z++) {
+	        const i = x * FLOOR.SIZE + z;
+	        const currentPoint = floor.geometry.vertices[i];
+
+	        if (currentPoint.y < HEIGHT_MAP[i]) {   // TODO: move to getIncrementValue ?
+                currentPoint.y += getIncrementValue(currentPoint.y, HEIGHT_MAP[i]);
+            }
+        }
+    }
+
+    floor.geometry.verticesNeedUpdate = true;
+
+    controls.update();
+	requestAnimationFrame(animate);
+	render();
+}
+
+function onWindowResize() {
+	camera.aspect = window.innerWidth / window.innerHeight;
+	camera.updateProjectionMatrix();
+
+	renderer.setSize(window.innerWidth, window.innerHeight);
+}
+
+init();
+animate();
+
+window.addEventListener("resize", onWindowResize);
